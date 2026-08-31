0c1e39cb3e5bc020bea5a54a5fec0bcf9e000962
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-31 23:06:00 +0530

    First code of express js

--- backendRevise/index.js
+++ backendRevise/index.js
@@ -1,10 +1,35 @@
-const htpp= require("http")
+const express = require('express')
 
-const myServer = htpp.createServer((req, res) =>{
-    console.log("Rec received");
-    res.end("Server ")
+const app = express();
+
+const port = 8000
+
+app.get("/", (req, res) =>{
+    res.send("Hello from home page")
+})
+app.get("/about", (req, res) =>{
+    res.send("Hello from about page")
 })
 
-myServer.listen(8000, () =>{
-    console.log("Server started")
+
+app.listen(port, () =>{
+    console.log("Server Started")
 })
+
+
+
+
+
+
+
+// const htpp= require("http")
+
+// const myServer = htpp.createServer((req, res) =>{
+//     console.log("Rec received");
+//     res.end("Server ")
+// })
+
+// myServer.listen(8000, () =>{
+//     console.log("Server started")
+// })
+
