611d0fad80cfe3d13e6606a8d8aca63da159a9ca
Sibtain Raza <khanbhai14063@gmail.com>
2026-09-15 20:49:45 +0530

    Server side rendering

--- Short_Url/index.js
+++ Short_Url/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const path = require("path");
+const URL = require("./models/url.models")
 const {connectDB}  = require("./config/db")
 const urlRoute = require("./routes/url.route");
+
 
 const app = express();
 const PORT = 8001;
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
+
+app.get("/test", async(req, res)=>{
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls,
+    })
+})
 
 connectDB("mongodb://localhost:27017/short-url");
 app.use("/url", urlRoute);
